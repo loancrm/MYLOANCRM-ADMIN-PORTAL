d28f910c2538498fd02c83e8bc59b0393edfc981
Mudhiiguubba kalyonnii <mudhiiguubbakalyonnii@gmail.com>
2026-03-04 22:58:34 +0530

    api modified

--- src/app/constants/project-constants.ts
+++ src/app/constants/project-constants.ts
@@ -1,13 +1,13 @@
 export const projectConstantsLocal = {
-
-    ACCOUNTS_STATUS: [
+  ACCOUNTS_STATUS: [
     { id: 0, name: 'all', displayName: 'All Accounts' },
     { id: 1, name: 'Active', displayName: 'Active Accounts' },
     { id: 2, name: 'In Active', displayName: 'In Active Accounts' },
   ],
-  
+
   // BASE_URL: 'http://localhost:5002/',
-  
-  BASE_URL: 'https://rest.thefintalk.in:5002/',
+
+  // BASE_URL: 'https://rest.thefintalk.in:5002/',
+  BASE_URL :'https://api.myloancrm.com/app/',
   VERSION_DESKTOP: '0.0.0',
 };
