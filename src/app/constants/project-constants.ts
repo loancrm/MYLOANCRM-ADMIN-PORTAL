769c95f1fc77710b82636c70befb704aae6eebbd
Mudhiiguubba kalyonnii <mudhiiguubbakalyonnii@gmail.com>
2026-05-28 10:48:17 +0530

    re deploy

--- src/app/constants/project-constants.ts
+++ src/app/constants/project-constants.ts
@@ -16,9 +16,8 @@ export const projectConstantsLocal = {
     { id: 1, name: 'active',   displayName: 'Active'   },
     { id: 2, name: 'inactive', displayName: 'Inactive' },
   ],
-  
-  // BASE_URL: 'http://localhost:5002/',
 
+  // BASE_URL: 'http://localhost:5002/',
   // BASE_URL: 'https://rest.thefintalk.in:5002/',
   BASE_URL :'https://api.myloancrm.com/app/',
   VERSION_DESKTOP: '0.0.0',
