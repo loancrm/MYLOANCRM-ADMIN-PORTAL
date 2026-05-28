fd2913caf680101d66b866854d72f91a2c70cf1b
dandamurisantharaju <dandamurisantharaju@gmail.com>
2026-05-28 10:17:37 +0530

    redeployed

--- src/app/constants/project-constants.ts
+++ src/app/constants/project-constants.ts
@@ -16,6 +16,7 @@ export const projectConstantsLocal = {
     { id: 1, name: 'active',   displayName: 'Active'   },
     { id: 2, name: 'inactive', displayName: 'Inactive' },
   ],
+  
   // BASE_URL: 'http://localhost:5002/',
 
   // BASE_URL: 'https://rest.thefintalk.in:5002/',
